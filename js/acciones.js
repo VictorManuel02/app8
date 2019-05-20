3e07cc03654d5c7039aa7613c64ca96f5d950c55
VictorManuel02 <48569469+VictorManuel02@users.noreply.github.com>
2019-05-20 14:00:40 -0500

    2

--- js/acciones.js
+++ js/acciones.js
@@ -5,7 +5,7 @@ $(document).ready(function (e){
 	
 });
 
-	function onDeviceready(){
+	function onDeviceReady(){
 		
 		$('#posicion').on('click',function(){
 			getPosition ();
@@ -18,7 +18,7 @@ $(document).ready(function (e){
 			maximumAge:3600000	
 		}
 		
-		var watchID= navigator.geolocation.getCurrentPosition(OnSuccess,onError,options);
+		var watchID=navigator.geolocation.getCurrentPosition(OnSuccess,onError,options);
 		
 		function onSuccess(position){
 			
